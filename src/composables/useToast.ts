ca7ecf3de25b4a530e31113dfa959740f6ab8e6f
Emmanuel Santiago <macbookpro@Emmanuel-Santiago.local>
2022-10-07 12:20:49 +0800

    create composable function

--- src/composables/useToast.ts
+++ src/composables/useToast.ts
@@ -1,3 +1,45 @@
+interface ToastProps {
+  type: string;
+  message: string | number;
+  style?: string;
+  isDisplay: boolean;
+}
+
 export function useToast() {
-    
+  const openToast = (toast: ToastProps) => {
+    const { type, message, isDisplay } = toast;
+    let style = "";
+    switch (type) {
+      case "error":
+        style = "bg-red-400";
+        break;
+      case "success":
+        style = "bg-green-400";
+        break;
+      default:
+        style = "bg-sky-400";
+    }
+    const config = {
+      type,
+      message: message ? message : `This is ${type}`,
+      style,
+      isDisplay,
+    };
+
+    return config;
+  };
+
+  const destroyToast = (toasts: any) => {
+    toasts.map((toast: ToastProps) => {
+      toast.isDisplay = false;
+    });
+    setTimeout(() => {
+      toasts.splice(0, 1);
+    }, 800);
+  };
+
+  return {
+    openToast,
+    destroyToast,
+  };
 }
